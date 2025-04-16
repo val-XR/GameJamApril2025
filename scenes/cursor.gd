892878ac6653ff13ee8a90b602cb515dbe9ede3e
val-XR <valxrstudios@gmail.com>
2025-04-16 18:12:46 -0400

    Working on tooltip

--- scenes/cursor.gd
+++ scenes/cursor.gd
@@ -10,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$".".global_position = get_global_mouse_position()
+	
+
+# detect scrap
