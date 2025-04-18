8c684242e0faed1864b4d50481c8e1a3ddaa427c
Jacob13044 <jacob13044@gmail.com>
2025-04-18 11:15:57 -0500

    Fixed Oxygen system

--- scenes/timer.gd
+++ scenes/timer.gd
@@ -2,23 +2,27 @@ extends Timer
 
 var Oxygen = 300
 var IsMoving = false
-var OldLocation: Vector2 # <-- declare here!
-
+var OldLocation: Vector2
 
 func _on_timeout() -> void:
 	if Oxygen <= 0:
 		pass # will do later
 	else:
-		if IsMoving:
-			Oxygen -= 2
+		# if IsMoving is true then uses oxygen faster
+		if IsMoving == true:
+			Oxygen -= 1.5
 		else:
+			# if IsMOving is false then uses oxygen normally
 			Oxygen -= 1
 		print(Oxygen)
 
+# Checks if the player is moving
 func _on_old_pos_timeout() -> void:
-	if OldLocation == %Player.global_position:
+	var player = $".."
+	# if OldLocation is equal to Current location
+	if OldLocation == player.global_position:
 		IsMoving = false
 	else:
+		# if Oldlocation is not equal to new location
 		IsMoving = true
-
-	OldLocation = %Player.global_position
+		OldLocation = player.global_position
