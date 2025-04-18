53098a28d4bfacd0b90a43f648b0d02ef673270b
val-XR <valxrstudios@gmail.com>
2025-04-18 03:41:55 -0400

    Started testing resource movement

--- scenes/Resources/resource_base.gd
+++ scenes/Resources/resource_base.gd
@@ -1,24 +1,27 @@
 extends Node2D
 
-var followPlayer: bool = false
+# Whether this resource should follow the player
+@export var followPlayer: bool = false
 
-@onready var player = %Player # player
-@onready var customResource: Node2D = $"."
+# A reference to the player node in the scene (can be assigned in editor)
+@export var player: Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# How quickly the resource moves toward the player
+@export var follow_speed: float = 30.0
 
+@export var minimum_distance: float = 50
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if followPlayer == true:
-		var targetPosition = player.global_posiiton
-	
-		# players position
-		var currentPosition = position
-	
-		# direction from player to cursor
-		var direction = targetPosition - currentPosition
-		var newPosition = direction + currentPosition
-		customResource.global_position = newPosition
+# Called every physics frame (used for movement/physics updates)
+func _physics_process(delta: float) -> void:
+	# Only execute follow logic if the flag is enabled and player is assigned
+	if followPlayer and player:
+		
+		minimum_distance = player.global_position.distance_to(global_position)
+		
+		# if object is far away
+		if minimum_distance > 50:
+			# Get direction from this node to the player
+			#var direction = (player.global_position - global_position)
+			var direction = (player.global_position - global_position).normalized()
+			# Move this node toward the player at the given speed
+			global_position += direction * follow_speed * delta
