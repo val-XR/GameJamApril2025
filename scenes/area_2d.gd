7028ffe6d036e68d2171f98e08425b93747f6b2c
Jacob13044 <jacob13044@gmail.com>
2025-04-24 00:03:02 -0500

    Working damage

--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -1,11 +1,14 @@
 extends Area2D
-var Test = false
+var InArea2D = false
 
 
 func _on_area_entered(area: Area2D) -> void:
-	GlobalPlayer.PlayerHealthAdd(-2)
-	Test = true
+	InArea2D = true
+
+func _on_area_exited(area: Area2D) -> void:
+	InArea2D = false
+
+func _on_timer_2_timeout() -> void:
+	if InArea2D:
+		GlobalPlayer.PlayerHealthAdd(-2)
 	
-func _process(delta: float) -> void:
-	if Test == true:
-		print(Test)
